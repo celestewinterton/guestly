2958e658ff7e527e0dfe5ff98dc6bffb22098cc7
Celeste Winterton <celestewinterton@gmail.com>
2022-04-28 13:50:31 -0700

    tested user auth log in route, working as expected

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,52 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-
-//TESTING FOR USER AUTH
-// // GET /api/set-token-cookie http://localhost:5000/api/set-token-cookie
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'demouser'
-//       }
-//     });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
-
-// // GET /api/restore-user http://localhost:5000/api/restore-user
-// const { restoreUser } = require('../../utils/auth.js');
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// // GET /api/require-auth http://localhost:5000/api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// console test to make sure api route works
-// fetch('/api/test', {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//   },
-//   body: JSON.stringify({ hello: 'world' })
-// }).then(res => res.json()).then(data => console.log(data));
 
 module.exports = router;
