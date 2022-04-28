0319bdf4e66af926b61d497c17da15f7922fb8e7
Celeste Winterton <celestewinterton@gmail.com>
2022-04-28 16:15:01 -0700

    added action creators for users

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
 import App from './App';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
