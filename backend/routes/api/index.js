b191da1c008134735a10fa25d651eae1ffc0696b
Celeste Winterton <celestewinterton@gmail.com>
2022-04-28 13:30:13 -0700

    testing worked for user auth api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,40 @@ router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
+// // GET /api/set-token-cookie http://localhost:5000/api/set-token-cookie
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'demouser'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // GET /api/restore-user http://localhost:5000/api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth http://localhost:5000/api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
 // console test to make sure api route works
 // fetch('/api/test', {
 //   method: "POST",
