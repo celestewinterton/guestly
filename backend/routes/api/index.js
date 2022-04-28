b1e1e8e897a95ad8b7e69da75e9e214a1382c0cd
Celeste Winterton <celestewinterton@gmail.com>
2022-04-28 10:23:08 -0700

    added basic error handling

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 
-
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
-
+// console test to make sure api route works
+// fetch('/api/test', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//   },
+//   body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
 
 module.exports = router;
