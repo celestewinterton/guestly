381210627fdc514ffc5e3b6699b9406bf30dadd9
Celeste Winterton <celestewinterton@gmail.com>
2022-04-28 17:06:57 -0700

    added some css for forms and buttons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import configureStore from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-import configureStore from './store';
-import App from './App';
 import * as sessionActions from './store/session';
 
 const store = configureStore();
